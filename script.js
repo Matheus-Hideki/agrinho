cfbf5a091af330d27e0adb7067a3e3f8ef41eba4
Matheus Hideki <104567107+Matheus-Hideki@users.noreply.github.com>
2023-07-25 09:56:37 -0300

    v2

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ menu()
     var audio = new Audio('Noodle Talk.mp3');
     audio.play();
 }
+//<div class="square-create" style="height: 10%;width: 100%; top: 0%; left:0%;background: linear-gradient(to bottom, #6f7be2 0%, #5662ca 100%);"></div>
+//background-color: #555; 
